0e430a6ac8ceddda5d917f0d91edb65bf3d47ee8
Diana Ward <diana.ward535@proton.me>
2026-01-19 10:56:52 -0700

    fix: 🐛 Rename file to chezemoi scheme

--- dot_config/hypr/scripts/executable_fabric-reload.sh
+++ dot_config/hypr/scripts/executable_fabric-reload.sh
@@ -3,6 +3,4 @@ pkill -f "Lunur-Shell"
 
 # Go to the script's directory before launching it
 cd ~/.config/Lunur-Shell || exit 1
-source venv/bin/activate
-sleep 0.1s
-python main.py &
+uv run main.py &
